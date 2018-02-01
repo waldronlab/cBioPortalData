3a5e3d0b7171ecbe7d030d8af684f7892193875d
LiNk-NY <mramos09@gmail.com>
2018-01-31 19:00:42 -0500

    include additional imports

--- R/MultiAssayExperimentData-pkg.R
+++ R/MultiAssayExperimentData-pkg.R
@@ -1,3 +1,4 @@
-#' @importFrom S4Vectors isSingleString metadata
+#' @importFrom S4Vectors isSingleString metadata SimpleList
+#' @importFrom SummarizedExperiment SummarizedExperiment
 #' @importFrom readr read_tsv
 NULL
