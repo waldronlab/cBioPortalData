6c310b7edea22c85f6a0ef9a07670b99564ac320
LiNk-NY <mramos09@gmail.com>
2018-02-02 18:22:46 -0500

    include additional import directives

--- R/MultiAssayExperimentData-pkg.R
+++ R/MultiAssayExperimentData-pkg.R
@@ -1,4 +1,6 @@
-#' @importFrom S4Vectors isSingleString metadata SimpleList
+#' @importFrom S4Vectors isSingleString metadata SimpleList DataFrame
 #' @importFrom SummarizedExperiment SummarizedExperiment
 #' @importFrom readr read_tsv
+#' @importFrom TCGAutils generateMap TCGAbarcode
+#' @importFrom MultiAssayExperiment MultiAssayExperiment
 NULL
