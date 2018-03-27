2ad63e1688f393acf3a25538ad45395f069cf703
LiNk-NY <mramos09@gmail.com>
2018-03-27 12:37:55 -0400

    add removeCache function

--- R/MultiAssayExperimentData-pkg.R
+++ R/MultiAssayExperimentData-pkg.R
@@ -5,5 +5,5 @@
 #' @importFrom TCGAutils generateMap TCGAbarcode
 #' @import RTCGAToolbox
 #' @importFrom BiocFileCache BiocFileCache bfcquery bfcadd bfcneedsupdate
-#' bfcdownload bfcrpath
+#' bfcdownload bfcrpath bfcremove
 NULL
