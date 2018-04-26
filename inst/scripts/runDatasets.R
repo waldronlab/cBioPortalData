17181902ad9eb5346cdb88aa5bfaa7a7b34cc516
LiNk-NY <mramos09@gmail.com>
2018-04-26 13:42:53 -0400

    update download script

--- inst/scripts/runDatasets.R
+++ inst/scripts/runDatasets.R
@@ -5,13 +5,5 @@ setCache("/data/16tb/cbio")
 data(studiesTable)
 full_ids <- studiesTable$cancer_study_id
 
-part_ids <- full_ids[full_ids != "blca_tcga"]
-
-data_links <- vapply(part_ids, downloadcBioPortal, character(1L))
-
-
-
-### Troubleshoot
-
-downloadcBioPortal("blca_tcga")
+data_links <- vapply(full_ids, downloadcBioPortal, character(1L))
 
