aeec4b19422bcc9200784bd401951a06fcf6dfe5
LiNk-NY <mramos09@gmail.com>
2018-06-01 16:17:58 -0400

    update studyTable file

--- inst/scripts/studyTableMeta.R
+++ inst/scripts/studyTableMeta.R
@@ -1,29 +1,14 @@
-library(cgdsr)
-library(S4Vectors)
+source("clean_case_list.R")
 
-cgds <- CGDS("http://www.cbioportal.org/")
+headerMap <- function(cancer_study_id, cgds_object) {
 
-clean_case_list <- function(study_id, cgds) {
-    caseList <-
-        getCaseLists(cgds, study_id)[, "case_list_description", drop = FALSE]
-    newDesc <- tidyr::separate(caseList, "case_list_description",
-        c("description", "samples"), sep = " \\((?=([0-9]))")
-    newDesc[, "samples"] <- trimws(gsub("samples\\)", "", newDesc[["samples"]],
-        ignore.case = TRUE))
-    S4Vectors::DataFrame(cancer_study_id = study_id, newDesc)
+    headermap <- readRDS(system.file("extdata/study_headers.rds",
+        package = "MultiAssayExperimentData", mustWork = TRUE))
+
+    id_table <- clean_case_list(cancer_study_id, cgds_object)
+
+    id_table$headers <- headermap$headers[
+        match(id_table$description, headermap$description)]
+    id_table[!duplicated(id_table$headers), ]
 }
-
-## use coad to get all possible fields
-coad <- clean_case_list("coadread_tcga_pub", cgds)
-
-## copy and paste headers from http://www.cbioportal.org/data_sets.jsp
-headers <- strsplit("All  Sequenced  CNA  RNA-Seq  Tumor mRNA (microarray)  Tumor miRNA  Methylation (HM27)  RPPA  Complete", "  ")[[1]]
-coadnums <- strsplit("276  224  257  244  224  85  236  196  195", "  ")[[1L]]
-names(coadnums) <- headers
-coad$headers <- names(coadnums)[match(coad$samples, coadnums)]
-
-headermap <- coad[, c("description", "headers")]
-headermap[complete.cases(headermap), ]
-
-prad <- clean_case_list("prad_tcga", cgds)
 
