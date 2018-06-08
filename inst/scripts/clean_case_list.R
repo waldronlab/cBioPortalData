01285b002643a55e88b15fad4c38c5e3d9081362
LiNk-NY <mramos09@gmail.com>
2018-06-08 16:19:41 -0400

    update regex for sample n extraction

--- inst/scripts/clean_case_list.R
+++ inst/scripts/clean_case_list.R
@@ -5,10 +5,11 @@ library(cgdsr)
 clean_case_list <- function(study_id, cgds) {
     caseList <-
         getCaseLists(cgds, study_id)[, "case_list_description", drop = FALSE]
-    newDesc <- tidyr::separate(caseList, "case_list_description",
-        c("description", "samples"), sep = " \\((?=([0-9]))")
-    newDesc[, "samples"] <- trimws(gsub("samples\\)", "", newDesc[["samples"]],
-        ignore.case = TRUE))
-    S4Vectors::DataFrame(cancer_study_id = study_id, newDesc)
+    nsamps <- as.numeric(stringr::str_match(caseList[["case_list_description"]],
+        "[0-9]{1,5}(?=\\ssamples)"))
+    newDESC <- gsub(" \\(*[0-9]{1,5} samples\\)*|\\.$", "",
+        caseList[["case_list_description"]])
+    S4Vectors::DataFrame(cancer_study_id = study_id, description = newDESC,
+        samples = nsamps)
 }
 
