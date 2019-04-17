3c552b4cd4090a6757bb54c7c5fb7a4e61ccd8d6
LiNk-NY <mramos09@gmail.com>
2019-04-17 19:40:52 -0400

    export exploratory functions

--- R/cBioPortal.R
+++ R/cBioPortal.R
@@ -7,13 +7,63 @@ cbioportal <- NULL
 #'
 #' @return An object of class 'cBioPortal'
 #'
+#' @importFrom AnVIL Service
+#'
 #' @export
 cBioPortal <- function() {
-    AnVIL::Service(
+    Service(
         service = "cBioPortal",
         host = "www.cbioportal.org",
         config = httr::config(ssl_verifypeer = 0L, ssl_verifyhost = 0L),
         authenticate_config = FALSE,
         package = "cBioPortalData"
     )
+}
+
+#' Obtain a table of studies and associated metadata
+#'
+#' @param cbio An object of class `cBioPortal`
+#'
+#' @export
+getStudies <- function(cbio) {
+    query <- cbio$getAllStudiesUsingGET()
+    studies <- httr::content(query)
+    studies <- lapply(studies, function(x) {
+        if (is.null(x[["pmid"]]))
+            x[["pmid"]] <- NA_character_
+        if (is.null(x[["citation"]]))
+            x[["citation"]] <- NA_character_
+        x
+    })
+    res <- do.call(rbind.data.frame, studies)
+    rownames(res) <- NULL
+    res
+}
+
+#' Obtain clinical data
+#'
+#' @param cbio An object of class `cBioPortal`
+#'
+#' @param studyId A single string indicating the "studyId" as taken from
+#'     `getStudies`
+#'
+#' @export
+clinicalData <- function(cbio, studyId = "acc_tcga") {
+    clinpost <- cbio$fetchAllClinicalDataInStudyUsingPOST(studyId = studyId)
+    clindat <- httr::content(clinpost)
+    dfclin <- do.call(rbind.data.frame, clindat)
+    cdat <- tidyr::spread(dfclin, clinicalAttributeId, value)
+    rownames(cdat) <- cdat[["sampleId"]]
+    cdat
+}
+
+#' Produce molecular profiles dataset
+#'
+#' @inheritParams clinicalData
+#'
+#' @export
+molecularProfiles <- function(cbio, studyId = "acc_tcga") {
+    mols <- cbio$getAllMolecularProfilesInStudyUsingGET(studyId = "acc_tcga")
+    cmols <- httr::content(mols)
+    do.call(rbind.data.frame, cmols)
 }
