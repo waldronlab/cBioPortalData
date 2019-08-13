89d31d3956fa62e810efd7526cb9f0c6cfe689f3
LiNk-NY <mramos09@gmail.com>
2019-08-13 16:34:00 -0400

    function overhauls, add sample functions

--- R/cBioPortal.R
+++ R/cBioPortal.R
@@ -26,7 +26,23 @@ cBioPortal <- function() {
     )
 }
 
-## curl -X POST "http://www.cbioportal.org/api/gene-panel-data/fetch" -H "accept: application/json" -H "Content-Type: application/json" -d "{ \"sampleMolecularIdentifiers\": [ { \"molecularProfileId\": \"acc_tcga_linear_CNA\", \"sampleId\": \"TCGA-OR-A5J1-01\" } ]}"
+.invoke_fun <- function(api, name, ...) {
+    ops <- names(operations(api))
+    if (!name %in% ops)
+        stop("<internal> operation name not found in API")
+    do.call(`$`, list(api, name))(...)
+}
+
+
+.bind_content <- function(x) {
+    dplyr::bind_rows(
+        httr::content(x)
+    )
+}
+
+.invoke_bind <- function(api, name, ...) {
+    .bind_content(.invoke_fun(api, name, ...))
+}
 
 #' Obtain a table of studies and associated metadata
 #'
@@ -66,10 +82,17 @@ clinicalData <- function(cbio, studyId = "acc_tcga") {
 #' @inheritParams clinicalData
 #'
 #' @export
-molecularProfiles <- function(cbio, studyId = "acc_tcga") {
-    mols <- cbio$getAllMolecularProfilesInStudyUsingGET(studyId = studyId)
+molecularProfiles <- function(cbio, studyId = "acc_tcga",
+    projection = c("SUMMARY", "ID", "DETAILED", "META"))
+{
+    projection <- match.arg(projection)
+    mols <- .invoke_fun(cbio, "getAllMolecularProfilesInStudyUsingGET",
+        studyId = studyId, projection = projection)
     cmols <- httr::content(mols)
-    dplyr::bind_rows(cmols)
+    if (projection %in% c("SUMMARY", "ID"))
+        dplyr::bind_rows(cmols)
+    else
+        cmols
 }
 
 #' Produce small dataset of molecular profile data
@@ -103,6 +126,22 @@ molecularSlice <- function(cbio, profileId = "acc_tcga_rna_seq_v2_mrna",
         sampleId, value)
 }
 
+#' Search through API operations
+#'
+#' Use a string to filter through all available operations
+#'
+#' @param cbio An object of class `cBioPortal`
+#' @param keyword A string for searching through available operations
+#'
+#' @examples
+#'
+#' searchOps(cbio, "mol")
+#'
+#' @export
+searchOps <- function(cbio, keyword) {
+    grep(keyword, names(operations(cbio)), value = TRUE, ignore.case = TRUE)
+}
+
 #' Get a table of all genes
 #'
 #' Query the API to get a list of Entrez Gene IDs, Hugo symbols, etc.
@@ -110,8 +149,8 @@ molecularSlice <- function(cbio, profileId = "acc_tcga_rna_seq_v2_mrna",
 #' @inheritParams clinicalData
 #'
 #' @export
-geneTable <- function(cbio) {
-    gres <- cbio$getAllGenesUsingGET()
+geneTable <- function(cbio, ...) {
+    gres <- cbio$getAllGenesUsingGET(...)
     glist <- httr::content(gres)
     glix <- lapply(glist, function(x) {
         if (is.null(x[["cytoband"]]))
@@ -135,78 +174,98 @@ geneTable <- function(cbio) {
 #' sampleLists
 #'
 #' @examples
-#' samplesInSampleLists(cb, sampleLists(cb))
+#' samplesInSampleLists(cc, "acc_tcga_rppa")
 #'
 #' @export
 samplesInSampleLists <- function(cbio, sampleListIds = c("acc_tcga_all")) {
     sampleListIds <- setNames(sampleListIds, sampleListIds)
-    cnames <- lapply(sampleListIds, function(x) {
+    meta <- structure(vector("list", length(sampleListIds)), .Names = sampleListIds)
+    res <- lapply(sampleListIds, function(x) {
         res <- cbio$getSampleListUsingGET(sampleListId = x)
-
         res2 <- httr::content(res)
-        meta <- res2[names(res2) != "sampleIds"]
-        ids <- unlist(res2[["sampleIds"]])
-        list(ids = ids, metadata = meta)
+        meta[[x]] <<- res2[names(res2) != "sampleIds"]
+        unlist(res2[["sampleIds"]])
     })
-    res <- IRanges::CharacterList(cnames[["ids"]])
-    metadata(res) <- cnames[["metadata"]]
+    res <- IRanges::CharacterList(res)
+    meta <- dplyr::bind_rows(meta)
+    metadata(res) <- meta
     res
 }
 
-#' Provide a sample lists within a study
+#' Provide sample lists for a particular study
 #'
 #' Given a particular `studyId``, this function will return all the available
 #' `sampleListId` identifiers
 #'
 #' @inheritParams clinicalData
 #'
+#' @examples
+#' sampleLists(cc, "acc_tcga")
+#'
 #' @export
 sampleLists <- function(cbio, studyId = "acc_tcga") {
-    slist <- cbio$getAllSampleListsInStudyUsingGET(studyId = studyId)
-    slist <- httr::content(slist)
-    dplyr::bind_rows(slist)
+    .invoke_bind(cbio, "getAllSampleListsInStudyUsingGET", studyId = studyId)
 }
 
 #' @export
-genePanel <- function(cbio, panelId = "NSCLC_UNITO_2016_PANEL") {
-   gp <- cbio$getGenePanelUsingGET(genePanelId = panelId)
-   gp <- httr::content(gp)
-   dplyr::bind_rows(gp$genes)
+allSamples <- function(cbio, studyId = "acc_tcga") {
+    .invoke_bind(cbio, "getAllSamplesInStudyUsingGET", studyId = studyId)
 }
 
-# genePanelMolecular <- function(cbio,
-#     molecularProfileId = "nsclc_unito_2016_mutations") {
-#
-#     (molprof <- molecularProfiles(cbio)$molecularProfileId)
-#     (samplist <- sampleLists(cbio)$sampleListId)
-# #    molprof <- "acc_tcga_linear_CNA"
-#     cbio$getGenePanelDataUsingPOST(
-#         list(
-#             sampleMolecularIdentifiers = data.frame(
-#                 molecularProfileId = "acc_tcga_rppa",
-#                 sampleListId = "acc_tcga_rppa"
-#             )
-#         )
-#     )
-#     getGP <- cbio$getGenePanelDataUsingPOST
-#
-#     samps <- head(unlist(samplesInSampleLists(cbio, molprof), use.names = FALSE))
-#
-#     attributes(getGP) <- NULL
-#     debugonce(getGP)
-#     getGP("acc_tcga_linear_CNA", sampleListId = "acc_tcga_cnaseq")
-#
-#     ## works !
-#     cbio$fetchGenePanelDataInMultipleMolecularProfilesUsingPOST
-#     httr:::content(
-#         cbio$fetchGenePanelDataInMultipleMolecularProfilesUsingPOST(
-#             sampleMolecularIdentifiers =
-#                 list( sampleMolecularIdentifiers =
-#                     data.frame(
-#                         molecularProfileId = "acc_tcga_linear_CNA",
-#                         sampleId = c("TCGA-OR-A5J1-01", "TCGA-OR-A5J2-01")
-#                     )
-#                 )
-#         )
-#     )
-# }
+#' @export
+genePanels <- function(cbio) {
+    .invoke_bind(cbio, "getAllGenePanelsUsingGET", studyId = studyId)
+}
+
+#' @export
+getGenePanel <- function(cbio, panelId = "NSCLC_UNITO_2016_PANEL") {
+    .invoke_bind(cbio, "getGenePanelUsingGET", genePanelId = panelId)
+}
+
+#' @export
+genePanelMolecular <-
+    function(cbio, molecularProfileId = "acc_tcga_linear_CNA",
+        sampleListId = "acc_tcga_cna", sampleIds = NULL)
+{
+    .invoke_bind(cbio, "getGenePanelDataUsingPOST",
+        molecularProfileId = molecularProfileId,
+        sampleListId = list(sampleListId = sampleListId)
+    )
+}
+
+#' @export
+getGenePanelMolecular <-
+    function(cbio, molecularProfileId = "acc_tcga_linear_CNA",
+        sampleIds = c("TCGA-OR-A5J1-01", "TCGA-OR-A5J2-01"))
+{
+    .invoke_bind(cbio,
+        "fetchGenePanelDataInMultipleMolecularProfilesUsingPOST",
+        sampleMolecularIdentifiers = list(sampleMolecularIdentifiers =
+            data.frame(
+                molecularProfileId = molecularProfileId,
+                sampleId = sampleIds
+            )
+        )
+    )
+}
+
+#' @export
+getSampleInfo <-
+    function(cbio, studyId = "acc_tcga", sampleListIds = NULL,
+        projection = c("SUMMARY", "ID", "DETAILED", "META"))
+{
+    projection <- match.arg(projection)
+    if (!is.null(sampleListIds))
+        queryobj <- list(sampleListIds = sampleListIds)
+    else
+        queryobj <- list(sampleIdentifiers =
+            as.data.frame(
+                allSamples(cbio, studyId)[, c("sampleId", "studyId")]
+            )
+        )
+
+    .invoke_bind(cbio, "fetchSamplesUsingPOST",
+        projection = projection, sampleIdentifiers = queryobj
+    )
+}
+
