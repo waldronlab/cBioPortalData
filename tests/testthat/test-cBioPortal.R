85392f5610228950838a0ef37374ef2a1b43664b
LiNk-NY <mramos09@gmail.com>
2020-06-12 10:24:14 -0400

    update api.yml, include consistency test

--- tests/testthat/test-cBioPortal.R
+++ tests/testthat/test-cBioPortal.R
@@ -1,8 +1,12 @@
-test_that("digesting different API works", {
-    expect_true(
-        !identical(
-            digest::digest(list(cBioPortal())),
-            digest::digest(list(cBioPortal("beta.cbioportal.org")))
-        )
+test_that("Check API is consistent with online resource", {
+    skip_if_offline()
+
+    api_url <- system.file(package = "cBioPortalData", "service",
+        "cBioPortal", "api.yaml", mustWork = TRUE)
+    tmp <- tempfile()
+    download.file("https://www.cbioportal.org/api/api-docs", destfile = tmp)
+    expect_equal(
+        digest::digest(api_url, file = TRUE),
+        digest::digest(tmp, file = TRUE)
     )
 })
