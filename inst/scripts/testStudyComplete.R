f0085bdde4b39d229920859d0e5b189c3932dc2d
LiNk-NY <marcel.ramosperez@roswellpark.org>
2020-07-02 22:32:43 -0400

    update testing script with latest rate from rda data

--- inst/scripts/testStudyComplete.R
+++ inst/scripts/testStudyComplete.R
@@ -1,6 +1,7 @@
 library(cBioPortalData)
 library(BiocFileCache)
 
+# setCache("/data/16tb/cbio")
 cbioportal <- cBioPortal()
 
 (studies <- getStudies(cbioportal)[["studyId"]])
@@ -9,21 +10,38 @@ complete <- vector("list", length(studies))
 names(complete) <- studies
 
 for (stud in studies) {
-    complete[[stud]] <- tryCatch({
-        cBioPortalData(cbioportal,
-                studyId = stud, genePanelId = "IMPACT341")
-    }, error = function(e) {
-        conditionMessage(e)
-    })
+    message("Working on: ", stud)
+    if (is.null(complete[[stud]])) {
+        complete[[stud]] <- tryCatch({
+            cBioPortalData(
+                cbioportal, studyId = stud, genePanelId = "IMPACT341"
+            )
+        }, error = function(e) conditionMessage(e))
+    }
 }
 
-save(complete, file = "maelistfromcbio.rda")
-load("maelistfromcbio.rda")
+# save(complete, file = "inst/scripts/maelistfromcbio.rda")
+load("inst/scripts/maelistfromcbio.rda")
+# load("maelistfromcbio.rda")
 
 chars <- vapply(complete, is.character, logical(1L))
 complete[chars]
 
 successrate <- (1 - length(complete[chars]) / length(complete) )* 100
 successrate
-## 92.5
+## 96.6
+
+maelengths <- lengths(
+    complete[
+        vapply(complete, function(x) is(x, "MultiAssayExperiment"), logical(1L))
+    ]
+)
+
+# save(maelengths, file = "maelenghtsfromcbio.rda")
+# load("maelengthsfromcbio.rda")
+
+table(maelengths)
+# maelengths
+#   1   2   3   4   5   6   7   8   9  10  11  12 13  14
+# 103  47  10  11  38   5  10  20   4   2   4   2  2   1
 
