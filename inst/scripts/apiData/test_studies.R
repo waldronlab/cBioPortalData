191d1b11d589caaa3381ec96b87a7ad33882a23b
LiNk-NY <mramos09@gmail.com>
2020-08-14 09:53:39 -0400

    apiData set up

--- inst/scripts/apiData/test_studies.R
+++ inst/scripts/apiData/test_studies.R
@@ -8,25 +8,26 @@ names(complete) <- studies
 
 for (stud in studies) {
     message("Working on: ", stud)
-    if (is.null(complete[[stud]])) {
-        complete[[stud]] <- tryCatch({
-            is(
-                cBioPortalData(
-                    cbioportal, studyId = stud, genePanelId = "IMPACT341"
-                ),
-                "MultiAssayExperiment"
+    complete[[stud]] <- is(
+        tryCatch({
+            cBioPortalData(
+                cbioportal, studyId = stud, genePanelId = "IMPACT341"
             )
-        }, error = function(e) conditionMessage(e))
-    }
+        }, error = function(e) conditionMessage(e)),
+        "MultiAssayExperiment"
+    )
 }
 
 denv <- new.env(parent = emptyenv())
 data("studiesTable", package = "cBioPortalData", envir = denv)
 studiesTable <- denv[["studiesTable"]]
 
-missingStudy <- studiesTable$cancer_study_id[!studiesTable$cancer_study_id %in% names(complete)]
+missingStudy <- studiesTable$cancer_study_id[
+    !studiesTable$cancer_study_id %in% names(complete)
+]
 if (length(missingStudy))
-    message("These datasets are not in the new API: ", paste0(missingStudy, collapse = ", "))
+    message("These datasets are not in the new API: ",
+        paste0(missingStudy, collapse = ", "))
 
 studiesTable[, "api_build"] <- complete[studiesTable$cancer_study_id]
 usethis::use_data(studiesTable, overwrite = TRUE)
