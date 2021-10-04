3995bbe5aca5e2305b4b39fb8b6cd6b002cbe474
LiNk-NY <marcel.ramosperez@roswellpark.org>
2021-10-04 12:10:16 -0400

    update changes to update_api-docs.R file

--- inst/scripts/update_api-docs.R
+++ inst/scripts/update_api-docs.R
@@ -1,4 +1,4 @@
-setwd("~/gh/cBioPortalData")
+# setwd("~/gh/cBioPortalData")
 file_loc <- "inst/service/cBioPortal/api.json"
 
 download.file(
@@ -12,5 +12,12 @@ mdline <- grep("\"[0-9a-f]{32}\"", cbiolines, value = TRUE)
 oldmd5 <- unlist(strsplit(mdline, "\""))[[2]]
 updatedlines <- gsub("\"[0-9a-f]{32}\"", dQuote(md5), cbiolines)
 
-if (!identical(oldmd5, md5))
+## success -- updated API files and MD5
+if (!identical(oldmd5, md5)) {
     writeLines(updatedlines, con = file("R/cBioPortal.R"))
+    quit(status = 0)
+} else {
+## failure -- API the same
+    quit(status = 1)
+}
+
