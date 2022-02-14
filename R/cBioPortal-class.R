150c2f1ea9e3299285dbf4cb6dc7298c16613977
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-02-14 18:31:02 -0500

    add cBioPortal-class.R file and move code

---
+++ R/cBioPortal-class.R
@@ -0,0 +1,56 @@
+.api_header <- function(x) x@api_header
+
+#' @name cBioPortal-class
+#'
+#' @title A class for representing the cBioPortal API protocol
+#'
+#' @description The `cBioPortal` class is a representation of the `cBioPortal`
+#'     API protocol that directly inherits from the `Service` class in the
+#'     `AnVIL` package. For more information, see the
+#'     \link[AnVIL:Service]{AnVIL} package.
+#'
+#' @details This class takes the static API as provided at
+#'     \url{https://www.cbioportal.org/api/api-docs} and creates an R object
+#'     with the help from underlying infrastructure (i.e.,
+#'     \link[rapiclient:rapiclient-package]{rapiclient} and
+#'     \link[AnVIL:Service]{AnVIL}) to give the user a unified representation
+#'     of the API specification provided by the cBioPortal group. Users are not
+#'     expected to interact with this class other than to use it as input
+#'     to the functionality provided by the rest of the package.
+#'
+#' @importFrom methods new
+#'
+#' @seealso  \link{cBioPortal}, \linkS4class{Service}
+#'
+#' @md
+#'
+#' @examples
+#'
+#' cBioPortal()
+#'
+#' @export
+.cBioPortal <- setClass(
+    "cBioPortal",
+    contains = "Service",
+    slots = c(api_header = "character")
+)
+
+#' @describeIn cBioPortal-class
+#'
+#' @importFrom AnVIL operations
+#' @importFrom methods callNextMethod
+#'
+#' @param x A \linkS4class{Service} instance or API representation as
+#'     given by the \link{cBioPortal} function.
+#'
+#' @inheritParams AnVIL::operations
+#'
+#' @export
+setMethod(
+    "operations", "cBioPortal",
+    function(x, ..., .deprecated = FALSE)
+{
+    callNextMethod(
+        x, .headers = .api_header(x), ..., .deprecated = .deprecated
+    )
+})
--- R/cBioPortal.R
+++ R/cBioPortal.R
@@ -1,38 +1,5 @@
 utils::globalVariables(c("clinicalAttributeId", "value", "sampleId"))
 
-#' @name cBioPortal-class
-#'
-#' @title A class for representing the cBioPortal API protocol
-#'
-#' @description The `cBioPortal` class is a representation of the cBioPortal
-#'     API protocol that directly inherits from the `Service` class in the
-#'     `AnVIL` package. For more information, see the 'AnVIL' package.
-#'
-#' @details This class takes the static API as provided at
-#'     \url{https://www.cbioportal.org/api/api-docs} and creates an R object
-#'     with the help from underlying infrastructure (i.e., 'rapiclient' and
-#'     'AnVIL') to give the user a unified representation of the API
-#'     specification provided by the cBioPortal group. Users are not
-#'     expected to interact with this class other than to use it as input
-#'     to the functionality provided by the rest of the package.
-#'
-#' @importFrom methods new
-#'
-#' @seealso  \link{cBioPortal}, \link[AnVIL]{Service}
-#'
-#' @examples
-#'
-#' cBioPortal()
-#'
-#' @export
-.cBioPortal <- setClass(
-    "cBioPortal",
-    contains = "Service",
-    slots = c(api_header = "character")
-)
-
-.api_header <- function(x) x@api_header
-
 .handle_token <- function(token) {
     if (file.exists(token))
         token <- readLines(token)
@@ -177,17 +144,6 @@ cBioPortal <- function(
         api_header = token
     )
 }
-
-#' @importFrom AnVIL operations
-#' @export
-setMethod(
-    "operations", "cBioPortal",
-    function(x, ..., .deprecated = FALSE)
-{
-    callNextMethod(
-        x, .headers = .api_header(x), ..., .deprecated = .deprecated
-    )
-})
 
 .loadReportData <- function() {
     denv <- new.env(parent = emptyenv())
